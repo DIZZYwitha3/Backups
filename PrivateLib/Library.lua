0fcb40ee403141c7ac0a54f0938b43f5b0a7ac96
DIZZY <164082301+DIZZYwitha3@users.noreply.github.com>
2024-06-25 11:06:50 +0200

    Update Library.lua

--- PrivateLib/Library.lua
+++ PrivateLib/Library.lua
@@ -1,7 +1,8 @@
 -- Taken from: https://raw.githubusercontent.com/Wuhazua/Private/main/UI%20Library
 
---// library script private
-getgenv().namehub = "Private"
+local LibraryModule = {}
+
+getgenv().namehub = "Banana"
 local UserInputService = game:GetService('UserInputService')
 local LocalPlayer = game:GetService('Players').LocalPlayer
 local TweenService = game:GetService('TweenService')
@@ -20,6 +21,7 @@ local Library = {
     dragging = false;
     drag_position = nil;
     start_position = nil;
+    namehub = "DefaultName";
 }
 
 
@@ -1347,174 +1349,6 @@ function Library:new()
     return Tab
 end
 
-local main = Library.new()
-local tab = main.create_tab('Main')
+LibraryModule.Library = Library
 
-tab.create_title({
-	name = 'AutoParry',
-	section = 'left'
-})
-tab.create_toggle({
-	name = 'Enabled',
-	flag = 'autoparry',
-
-	section = 'left',
-	enabled = false,
-
-	callback = function(state: boolean)
-		print(`{state}`)
-	end
-})
-tab.create_dropdown({
-	name = 'Direction',
-	flag = 'Direction',
-	section = 'left',
-
-	option = 'Custom',
-	options = {'Custom', 'High', 'Random'},
-
-	callback = function(value: string)
-		print(value)
-	end
-})
-tab.create_title({
-	name = 'Visualizer',
-	section = 'right'
-})
-tab.create_toggle({
-	name = 'Enabled',
-	flag = 'visualise',
-
-	section = 'right',
-	enabled = false,
-
-	callback = function(state: boolean)
-	print(`{state}`)
-	end
-})
-tab.create_title({
-	name = 'HitSound',
-	section = 'left'
-})
-tab.create_toggle({
-	name = 'Enabled',
-	flag = 'hitsound',
-
-	section = 'left',
-	enabled = false,
-
-	callback = function(state: boolean)
-		print(`{state}`)
-	end
-})
-tab.create_dropdown({
-	name = 'Sound',
-	flag = 'soundpick',
-	section = 'left',
-
-	option = 'Neverlose',
-	options = {'Click', 'Neverlose', 'Bonk'},
-
-	callback = function(value: string)
-		print(value)
-	end
-})
-tab.create_title({
-	name = 'AI',
-	section = 'right'
-})
-tab.create_toggle({
-	name = 'Enabled',
-	flag = 'AI',
-
-	section = 'right',
-	enabled = false,
-
-	callback = function(state: boolean)
-    print(`{state}`)
-	end
-})
-tab.create_title({
-	name = 'Spin',
-	section = 'right'
-})
-tab.create_toggle({
-	name = 'Enabled',
-	flag = 'spin',
-
-	section = 'right',
-	enabled = false,
-
-	callback = function(state: boolean)
-		print(`{state}`)
-	end
-})
-tab.create_slider({
-	name = 'Speed',
-	flag = 'spinspeed',
-
-	section = 'right',
-
-	value = 25,
-	minimum_value = 0,
-	maximum_value = 100,
-
-	callback = function(value: number)
-		print(value)
-	end
-})
-tab.create_title({
-	name = 'Auto Open Crate',
-	section = 'left'
-})
-tab.create_toggle({
-	name = 'Enabled',
-	flag = 'swordbox',
-
-	section = 'left',
-	enabled = false,
-
-	callback = function(state: boolean)
-		print(`{state}`)
-	end
-})
-tab.create_dropdown({
-	name = 'Crate',
-	flag = 'selectbox',
-	section = 'left',
-
-	option = 'Sword Crate',
-	options = {'Explosion Crate', 'Sword Crate'},
-
-	callback = function(value: string)
-		print(value)
-	end
-})
-tab.create_title({
-	name = 'Auto Rewards',
-	section = 'right'
-})
-tab.create_toggle({
-	name = 'Enabled',
-	flag = 'rewarde',
-
-	section = 'right',
-	enabled = false,
-
-	callback = function(state: boolean)
-		print(`{state}`)
-	end
-})
-tab.create_dropdown({
-	name = 'Rewards',
-	flag = 'selectrewards',
-	section = 'right',
-
-	option = 'Playtime Rewards',
-	options = {'Playtime Rewards', 'Clan Rewards', 'Login Rewards', 'All'},
-
-	callback = function(value: string)
-	    print(value)
-	end
-})
-return library
+return LibraryModule
